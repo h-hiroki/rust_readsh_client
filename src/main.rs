8222e0bdc3c675c4217ca76e3e9a2f8b02846164
hhiroki <gyouzakuuka@gmail.com>
2020-05-17 21:52:36 +0900

    call any endpoint

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,15 @@ mod app_config;
 
 use crate::app_config::get_config;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
     let cnf = get_config();
-    println!("{}", cnf.base_url);
-    println!("{}", cnf.api_key);
+
+    // とりあえずAPI叩いてみる
+    let url = format!("{}{}{}", cnf.base_url, "/api/queries/2168?api_key=", cnf.api_key);
+    let resp = reqwest::get(&url).await?;
+    println!("Status: {}", resp.status());
+    let body = resp.text().await?;
+    println!("Body:\n\n{}", body);
+    Ok(())
 }
